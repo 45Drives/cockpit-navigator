ee6fc26ffcd5f26cfb9f4bbbfd83332aa91219d5
joshuaboud <joshuaboud@hotmail.com>
2021-06-03 13:34:52 -0300

    implement drag and drop file upload

--- navigator/navigator.js
+++ navigator/navigator.js
@@ -837,6 +837,180 @@ class NavContextMenu {
 	}
 }
 
+class FileUpload {
+	/**
+	 * 
+	 * @param {File|Blob} file 
+	 * @param {Number} chunk_size 
+	 * @param {NavWindow} nav_window_ref
+	 */
+	constructor(file, chunk_size, nav_window_ref) {
+		this.chunk_size = chunk_size;
+		this.filename = file.name;
+		this.nav_window_ref = nav_window_ref;
+		this.path = nav_window_ref.pwd().path_str() + "/" + file.name;
+		this.reader = new FileReader();
+		this.chunks = this.slice_file(file);
+		this.chunk_index = 0;
+	}
+
+	make_html_element() {
+		var notification = document.createElement("div");
+		notification.classList.add("nav-notification");
+		var header = document.createElement("div");
+		header.classList.add("nav-notification-header");
+		notification.appendChild(header);
+		header.innerText = "Uploading " + this.filename;
+		var progress = document.createElement("progress");
+		progress.max = this.num_chunks;
+		notification.appendChild(progress);
+		this.progress = progress;
+		this.html_elements = [progress, header, notification];
+		document.getElementById("nav-notifications").appendChild(notification);
+	}
+
+	remove_html_element() {
+		for (let elem of this.html_elements) {
+			elem.parentElement.removeChild(elem);
+		}
+	}
+
+	/**
+	 * 
+	 * @param {File|Blob} file 
+	 * @returns {Array}
+	 */
+	slice_file(file) {
+		var offset = 0;
+		var chunks = [];
+		this.num_chunks = Math.ceil(file.size / this.chunk_size);
+		for (let i = 0; i < this.num_chunks; i++) {
+			var next_offset = Math.min(this.chunk_size * (i + 1), file.size);
+			chunks.push(file.slice(offset, next_offset));
+			offset = next_offset;
+		}
+		return chunks;
+	}
+
+	upload() {
+		this.make_html_element();
+		this.proc = cockpit.spawn(["/usr/share/cockpit/navigator/scripts/write-chunk.py", this.path], {err: "out", superuser: "try"});
+		this.proc.fail((e, data) => {
+			window.alert(data);
+		})
+		this.proc.done((data) => {
+			this.nav_window_ref.stop_load();
+		})
+		this.reader.onload = (function(uploader_ref) {
+			return async function(evt) {
+				uploader_ref.write_to_file(evt, uploader_ref.chunk_index * uploader_ref.chunk_size);
+				uploader_ref.chunk_index++;
+				uploader_ref.progress.value = uploader_ref.chunk_index;
+				if (uploader_ref.chunk_index < uploader_ref.num_chunks)
+					uploader_ref.reader.readAsArrayBuffer(uploader_ref.chunks[uploader_ref.chunk_index]);
+				else {
+					uploader_ref.done();
+				}
+			};
+		})(this);
+		this.reader.readAsArrayBuffer(this.chunks[0]);
+	}
+
+	arrayBufferToBase64(buffer) {
+		let binary = '';
+		let bytes = new Uint8Array(buffer);
+		let len = bytes.byteLength;
+		for (let i = 0; i < len; i++) {
+			binary += String.fromCharCode(bytes[i]);
+		}
+		return window.btoa(binary);
+	}
+
+	/**
+	 * 
+	 * @param {Event} evt 
+	 * @param {Number} offset 
+	 */
+	write_to_file(evt, offset) {
+		var chunk_b64 = this.arrayBufferToBase64(evt.target.result);
+		const seek = this.chunk_index * this.chunk_size;
+		var obj = {
+			seek: seek,
+			chunk: chunk_b64
+		};
+		this.proc.input(JSON.stringify(obj) + "\n", true);
+	}
+
+	done() {
+		this.proc.input(); // close stdin
+		this.nav_window_ref.refresh();
+		this.remove_html_element();
+	}
+}
+
+class NavDragDrop {
+	/**
+	 * 
+	 * @param {HTMLDivElement} drop_area 
+	 * @param {NavWindow} nav_window_ref 
+	 */
+	constructor(drop_area, nav_window_ref) {
+		drop_area.addEventListener("dragenter", this);
+		drop_area.addEventListener("dragover", this);
+		drop_area.addEventListener("dragleave", this);
+		drop_area.addEventListener("drop", this);
+		this.drop_area = drop_area;
+		this.nav_window_ref = nav_window_ref;
+	}
+
+	handleEvent(e) {
+		e.preventDefault();
+		switch(e.type){
+			case "dragenter":
+				this.drop_area.classList.add("drag-enter");
+				break;
+			case "dragover":
+				break;
+			case "dragleave":
+				this.drop_area.classList.remove("drag-enter");
+				break;
+			case "drop":
+				if (e.dataTransfer.items) {
+					for (let item of e.dataTransfer.items) {
+						if (item.kind === 'file') {
+							var file = item.getAsFile();
+							var uploader = new FileUpload(file, 4096, this.nav_window_ref);
+							uploader.upload();
+							// reader.onloadend = (function(file_, nav_window_ref) {
+							// 	return function(evt) {
+							// 		nav_window_ref.upload(evt, file_);
+							// 	};
+							// })(file, this.nav_window_ref);
+							// reader.readAsArrayBuffer(file);
+						}
+					}
+				} else {
+					for (let file of ev.dataTransfer.files) {
+						var uploader = new FileUpload(file, 4096, this.nav_window_ref);
+						uploader.upload();
+						// reader.onloadend = (function(file_, nav_window_ref) {
+						// 	return function(evt) {
+						// 		nav_window_ref.upload(evt, file_);
+						// 	};
+						// })(file, this.nav_window_ref);
+						// reader.readAsArrayBuffer(file);
+					}
+				}
+				this.drop_area.classList.remove("drag-enter");
+				break;
+			default:
+				this.drop_area.classList.remove("drag-enter");
+				break;
+		}
+		e.stopPropagation();
+	}
+}
+
 class NavWindow {
 	constructor() {
 		this.path_stack = (localStorage.getItem('navigator-path') ?? '/').split('/');
@@ -853,6 +1027,8 @@ class NavWindow {
 		this.context_menu = new NavContextMenu("nav-context-menu", this);
 
 		this.clip_board = [];
+
+		this.uploader = new NavDragDrop(this.window, this);
 	}
 
 	/**
@@ -1386,6 +1562,27 @@ class NavWindow {
 		}
 		document.getElementById("pwd").disabled = false;
 	}
+
+	// /**
+	//  * 
+	//  * @param {Event} event 
+	//  * @param {*} file 
+	//  */
+	// async upload(event, file) {
+	// 	var bytes = new Uint8Array(event.target.result);
+	// 	console.log(bytes, file);
+	// 	var out_path = this.pwd().path_str() + "/" + file.name;
+	// 	var proc = cockpit.spawn(["dd", "of=" + out_path], {superuser: "try", err: "out"});
+	// 	// for (let i = 0; i < bytes.byteLength; i++) {
+	// 	// 	proc.input(bytes[i], true);
+	// 	// }
+	// 	proc.input(bytes);
+	// 	proc.fail((e, data) => {
+	// 		window.alert(data);
+	// 	});
+	// 	await proc;
+	// 	this.refresh();
+	// }
 }
 
 let nav_window = new NavWindow();
