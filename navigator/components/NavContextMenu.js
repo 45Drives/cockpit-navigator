946459c07bf4d1fb87a88a46c7fb9c672f3d0813
joshuaboud <joshuaboud@hotmail.com>
2021-07-15 14:45:42 -0300

    add copyright headers to source files

--- navigator/components/NavContextMenu.js
+++ navigator/components/NavContextMenu.js
@@ -1,3 +1,22 @@
+/* 
+	Cockpit Navigator - A File System Browser for Cockpit.
+	Copyright (C) 2021 Josh Boudreau      <jboudreau@45drives.com>
+	Copyright (C) 2021 Sam Silver         <ssilver@45drives.com>
+	Copyright (C) 2021 Dawson Della Valle <ddellavalle@45drives.com>
+
+	This file is part of Cockpit Navigator.
+	Cockpit Navigator is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	Cockpit Navigator is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with Cockpit Navigator.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import {NavEntry} from "./NavEntry.js";
 import {NavFile, NavFileLink} from "./NavFile.js";
 import {NavDir, NavDirLink} from "./NavDir.js";
