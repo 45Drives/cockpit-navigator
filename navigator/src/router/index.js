3802bcf1c7cc585fc60ad16dbf018ef39557db1f
joshuaboud <joshuaboud@hotmail.com>
2022-05-30 13:19:51 -0300

    add host parameter

--- navigator/src/router/index.js
+++ navigator/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
 	{
-		path: '/browse:path(/.*)?',
+		path: '/browse:host(/[^/:]+:)?:path(/.*)?',
 		name: 'browse',
 		component: () => import('../views/Browser.vue'),
 	},
