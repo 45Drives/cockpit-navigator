4dfc7ce73214cb5da43694b1e6b20f7e9cd68d2e
joshuaboud <joshuaboud@hotmail.com>
2022-08-23 18:19:20 -0300

    add path aliasing

--- navigator/vite.config.ts
+++ navigator/vite.config.ts
@@ -1,15 +1,21 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 	base: "./",
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		},
+	},
 	build: {
 		target: [
 			"chrome87", "edge88", "firefox78", "safari14"
-		]
+		],
 	},
 	test: {
 		globals: true,
